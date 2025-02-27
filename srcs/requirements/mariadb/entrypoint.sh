09d74a70f54aaef375622d3c8b9f497a0425c012
tobeshota <Shota.Tobe@keio.jp>
2025-02-27 10:04:23 +0900

    feat: specify `init_file` in `my.cnf` to simplify the script to execute initialization file

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -9,20 +9,8 @@ if [ ! -f /tmp/init_substituted.sql ]; then
     # mysql_install_db を使用して、MySQL/MariaDBのデータディレクトリを初期化
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-    # ネットワーク通信を無効化して、MySQLサーバをバックグラウンドで起動
-    mysqld_safe --skip-networking &
-    pid="$!"  # バックグラウンドプロセスのプロセスIDを保存
-    sleep 10  # サーバが完全起動するまで待機
-
-    # init.sql内の環境変数を envsubst で置換し、一時ファイルに出力
+    # init.sql内の環境変数を envsubst で置換し、my.cnfでinit_file指定のファイルに出力
     envsubst < /tmp/init.sql > /tmp/init_substituted.sql
-
-    # 置換済みSQLスクリプトを実行して、初期化処理（DB作成やユーザー設定など）を実施
-    mysql --user=mysql < /tmp/init_substituted.sql
-
-    # 初期化完了後、MySQLサーバをシャットダウン
-    mysqladmin --user=mysql shutdown
-    wait $pid  # バックグラウンドプロセスの終了を待機
 fi
 
 # 初期化済みの場合は、通常の起動方法でMySQLサーバを実行
