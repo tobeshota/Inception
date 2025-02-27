7db59a4ec2ecdff3e11e0eb7e931cca880e21cd2
tobeshota <Shota.Tobe@keio.jp>
2025-02-27 09:46:08 +0900

    feat: enable to specify the information about mariadb from

--- srcs/requirements/mariadb/init.sql
+++ srcs/requirements/mariadb/init.sql
@@ -1,12 +1,12 @@
 -- DBの初期値データとして実行するSQL．
 -- データベースの作成
-CREATE DATABASE IF NOT EXISTS wordpress_db;
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
 
 -- ユーザの追加
-CREATE USER IF NOT EXISTS 'wordpress_user'@'%' IDENTIFIED BY 'password'; -- '%'は任意のIPアドレスから接続を許可する
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}'; -- '%'は任意のIPアドレスから接続を許可する
 
 -- ユーザーにデータベースへの権限を付与
-GRANT ALL PRIVILEGES ON wordpress_db.* TO 'wordpress_user'@'%';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 
 -- 権限の反映
 FLUSH PRIVILEGES;
