0b1bc6a05d7590d1365120a01defc971608765cc
tobeshota <Shota.Tobe@keio.jp>
2025-02-27 10:41:38 +0900

    feat: Update the mariaDB server instead of initializing it if there is an existing data directory

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,17 +1,51 @@
 #!/bin/bash
 set -e
 
-# データベースの初期化チェック:
-# /tmp/init_substituted.sqlが存在しない場合は初回起動とみなして初期化処理を実行
-if [ ! -f /tmp/init_substituted.sql ]; then
-    echo "Initializing database..."
+# /tmp/init_substituted.sqlが存在しない場合は初回起動とみなして初期化処理を実行する
+init_database() {
+    if [ ! -f /tmp/init_substituted.sql ]; then
+        echo "Initializing database..."
+        # init.sql内の環境変数を envsubst で置換し、my.cnfでinit_file指定のファイルに出力
+        envsubst < /tmp/init.sql > /tmp/init_substituted.sql
+    else
+        echo "Database already initialized."
+    fi
+}
 
-    # mysql_install_db を使用して、MySQL/MariaDBのデータディレクトリを初期化
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+# mariadbを更新または初期化する
+install_mariadb() {
+    if [! ls /var/lib/mysql/mysql/user.* > /dev/null 2>&1]; then
+        # 既存のデータディレクトリ（/var/lib/mysql）にシステムテーブル（特にmysql.user）が存在する場合，
+        # テーブルの互換性をmariadbサーバのバージョンと合わせること（mysql_upgrade）が必要となる．
+        # mysql_upgradeはMariaDBサーバが稼働している状態で実行する必要があるため，
+        # バックグランドでサーバを起動している．
 
-    # init.sql内の環境変数を envsubst で置換し、my.cnfでinit_file指定のファイルに出力
-    envsubst < /tmp/init.sql > /tmp/init_substituted.sql
-fi
+        # mariaDBサーバのプロセスをバックグラウンドで起動
+        mysqld --skip-networking --socket=/run/mysqld/mysqld.sock &
+        pid="$!"
+        # ソケットが作成されるのを待つ（mysql_upgradeにはソケットでの接続が必要となるため）
+        until mysqladmin --socket=/run/mysqld/mysqld.sock ping --silent; do
+            sleep 1
+        done
+        echo "Running mysql_upgrade..."
+        mysql_upgrade --user=mysql
+        # mariaDBサーバのプロセスを終了
+        echo "Shutting down temporary server..."
+        mysqladmin --socket=/run/mysqld/mysqld.sock shutdown
+        # mariaDBサーバのプロセス終了を待つ
+        wait "$pid"
+    else
+        # MariaDBのデータディレクトリを初期化
+        echo "Running mysql_install_db..."
+        mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    fi
+}
 
-# 初期化済みの場合は、通常の起動方法でMySQLサーバを実行
-exec mysqld
+main() {
+    install_mariadb
+    init_database
+    # MariaDBサーバを実行
+    exec mysqld
+}
+
+main
