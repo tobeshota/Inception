8187e5a77e55d2dad4467d96e96557dfb8d734f9
tobeshota <Shota.Tobe@keio.jp>
2025-03-01 19:38:05 +0900

    feat: use something to be secret from `.env`

--- srcs/requirements/wordpress/wp-config.php
+++ srcs/requirements/wordpress/wp-config.php
@@ -24,24 +24,19 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-// define( 'DB_NAME', getenv('DB_NAME') );
-define( 'DB_NAME', 'wpdb' );
+define( 'DB_NAME', getenv('DB_NAME') );
 
 /** Database username */
-// define( 'DB_USER', getenv('DB_USER') );
-define( 'DB_USER', 'wpdb_user' );
+define( 'DB_USER', getenv('DB_USER') );
 
 /** Database password */
-// define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
-define( 'DB_PASSWORD', 'hoge' );
+define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
 
 /** Database hostname */
-// define( 'DB_HOST', getenv('DB_HOST') );
-define( 'DB_HOST', 'mariadb' );
+define( 'DB_HOST', getenv('DB_HOST') );
 
 /** Database charset to use in creating database tables. */
-// define( 'DB_CHARSET', getenv('DB_CHARSET') );
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', getenv('DB_CHARSET') );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
