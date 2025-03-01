735074a6bd1096d582e61cf3448b7ab434eef1bc
tobeshota <Shota.Tobe@keio.jp>
2025-03-01 16:57:59 +0900

    fix: run init.sql by root

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -14,25 +14,28 @@ init_database() {
 # mariadbを更新または初期化する
 install_mariadb() {
     if ls /var/lib/mysql/mysql/user.* > /dev/null 2>&1; then
+        echo "Already initialized."
+
+
         # 既存のデータディレクトリ（/var/lib/mysql）にシステムテーブル（特にmysql.user）が存在する場合，
         # テーブルの互換性をmariadbサーバのバージョンと合わせること（mysql_upgrade）が必要となる．
         # mysql_upgradeはMariaDBサーバが稼働している状態で実行する必要があるため，
         # バックグランドでサーバを起動している．
 
-        # mariaDBサーバのプロセスをバックグラウンドで起動
-        mysqld --skip-networking --socket=/run/mysqld/mysqld.sock &
-        pid="$!"
-        # ソケットが作成されるのを待つ（mysql_upgradeにはソケットでの接続が必要となるため）
-        until mysqladmin --socket=/run/mysqld/mysqld.sock ping --silent; do
-            sleep 1
-        done
-        echo "Running mysql_upgrade..."
-        mysql_upgrade --user=mysql
-        # mariaDBサーバのプロセスを終了
-        echo "Shutting down temporary server..."
-        mysqladmin --socket=/run/mysqld/mysqld.sock shutdown
-        # mariaDBサーバのプロセス終了を待つ
-        wait "$pid"
+        # # mariaDBサーバのプロセスをバックグラウンドで起動
+        # mysqld --skip-networking --socket=/run/mysqld/mysqld.sock &
+        # pid="$!"
+        # # ソケットが作成されるのを待つ（mysql_upgradeにはソケットでの接続が必要となるため）
+        # until mysqladmin --socket=/run/mysqld/mysqld.sock ping --silent; do
+        #     sleep 1
+        # done
+        # echo "Running mysql_upgrade..."
+        # mysql_upgrade --user=mysql
+        # # mariaDBサーバのプロセスを終了
+        # echo "Shutting down temporary server..."
+        # mysqladmin --socket=/run/mysqld/mysqld.sock shutdown
+        # # mariaDBサーバのプロセス終了を待つ
+        # wait "$pid"
     else
         # MariaDBのデータディレクトリを初期化
         echo "Running mysql_install_db..."
--- srcs/requirements/mariadb/init.sql
+++ srcs/requirements/mariadb/init.sql
@@ -1,12 +1,4 @@
--- DBの初期値データとして実行するSQL．
--- データベースの作成
 CREATE DATABASE IF NOT EXISTS ${DB_NAME};
-
--- ユーザの追加
-CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}'; -- '%'は任意のIPアドレスから接続を許可する
-
--- ユーザーにデータベースへの権限を付与
-GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
-
--- 権限の反映
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
